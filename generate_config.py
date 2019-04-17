f57807a9ac9b2332a34adffd587024a8da431aa6
Gabriel Bretschner <gabriel@lilt.com>
2019-04-17 15:07:41 +0200

    allow multiple keys to be modified

--- generate_config.py
+++ generate_config.py
@@ -10,32 +10,48 @@ except ImportError:
 def update_dict(the_dict, keys, value):
     if len(keys) == 0:
         return value
-
-    for k,v in the_dict.items():
-        if k == keys[0]:
-            the_dict[k] = update_dict(v, keys[1:], value)
+    if isinstance(the_dict, list):
+        keys[0] = int(keys[0])
+        
+    the_dict[keys[0]] = update_dict(the_dict[keys[0]], keys[1:], value)
     return the_dict
 
 
 @click.command()
 @click.option('--template', required=True, help='Template config in yaml format.', type=click.File())
 @click.option('--output', required=True, help="Output base-filename", type=click.Path(exists=False))
-@click.option('--key', required=True, help="the key to replace (use . to delimit levels)", type=click.STRING)
-@click.option('--values', required=True, help='the values to set it to', type=ListClickType())
+@click.option('--key', required=True, multiple=True, help="the key to replace (use . to delimit levels)", type=click.STRING)
+@click.option('--values', required=True, multiple=True, help='the values to set it to', type=ListClickType())
 @click.option('--names', default=" ", help="Name for each value for file name", type=ListClickType())
 def generate_config(template, output, key, values, names):
     """Generate new config from template by replacing given key with values."""
-    if len(names) > 0:
-        assert len(values) == len(names), "each value needs a name specified"
+    print(locals())
+    if isinstance(key, tuple):
+        assert isinstance(values, tuple) and len(key) == len(values)
+        key = list(key)
+        values = list(values)
     else:
-        names = values
+        key = [key]
+        values = [values]
+
+    if len(names) > 0:
+        for v in values:
+            assert len(v) == len(names), "each value needs a name specified"
+    else:
+        names = values[0]
 
     config_template = load(template, Loader=Loader)
-    keys = key.split('.')
-    for name, value in zip(names, values):
-        new_config = update_dict(config_template, keys, value)
+    configs = [None] * len(names)
+    for k, v in zip(key, values):
+        keys = k.split('.')
+        for i, value in enumerate(v):
+            print(config_template)
+            new_config = update_dict(config_template.copy(), keys, value)
+            configs[i] = new_config
+
+    for name, config in zip(names, configs):
         with open("%s.%s.yaml" % (output, name), "w") as f:
-            f.write(dump(new_config, explicit_start=True))
+            f.write(dump(config, explicit_start=True))
 
 
 if __name__ == '__main__':
