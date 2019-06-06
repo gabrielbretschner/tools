e4d9437e31fdefc51897791b6ab71cdb6fb24820
Gabriel Bretschner <gabriel@lilt.com>
2019-06-06 13:59:35 +0200

    test GPU Speed as well as CPU speed

--- test_translate.sh
+++ test_translate.sh
@@ -10,6 +10,15 @@ REFERENCE=$2
 TARGET_BPE=$3
 git_hash=$4
 
+function has_gpu(){
+	if ! [ -x "$(command -v nvidia-smi)" ]; then
+		has_gpu=false
+	else
+		has_gpu=true
+	fi
+	export has_gpu
+}
+
 function config_hash(){
 	echo $(sha256sum "$NEURAL_CONFIG" | cut -d' ' -f1)
 }
@@ -52,12 +61,19 @@ function run_experiment(){
 	mode=$1
 	beam_size=$2
 	batch_size=$3
+	processor=$4
 	if [[ ! ("$mode" != "greedy" || ("$batch_size" == 1 && "$beam_size" == 1)) ]]; then
 		echo "greedy mode requires batch 1 and beam 1"
 		exit 1
 	fi
+	has_gpu
+	if [[ $has_gpu && "$processor" == "cpu" ]]; then
+	 	export CUDA_VISIBLE_DEVICES=
+	 else
+		export CUDA_VISIBLE_DEVICES=0
+	 fi 
 	echo "translate $mode.$beam_size.$batch_size"
-	log_file=$(run_translate "${result_path}/${mode}.beam.${beam_size}.batch.${batch_size}.${git_hash}" "$mode" "translate.batch-size=${batch_size} translate.beam-size=${beam_size}")
+	log_file=$(run_translate "${result_path}/${mode}.${processor}.beam.${beam_size}.batch.${batch_size}.${git_hash}" "$mode" "translate.batch-size=${batch_size} translate.beam-size=${beam_size}")
 	echo "done"
 	ws=$(grep "words/sec" "$log_file" | cut -d' ' -f11,12)
 	bleu=$(grep BLEU "$log_file" | cut -d= -f2 |cut -d' ' -f2)
@@ -110,12 +126,19 @@ python_env_hash
 title="command used: $0 $*\ngit-hash: $git_hash \nconfig hash: $(config_hash)\npython hash: ${python_hash}"
 result_string=""
 
-run_experiment "greedy" "1" "1"
-run_experiment "beam" "1" "1"
-run_experiment "beam" "1" "100"
-run_experiment "beam" "2" "1"
-run_experiment "beam" "2" "100"
-
+run_experiment "greedy" "1" "1" "cpu"
+run_experiment "beam" "1" "1" "cpu"
+run_experiment "beam" "1" "100" "cpu"
+run_experiment "beam" "2" "1" "cpu"
+run_experiment "beam" "2" "100" "cpu"
+has_gpu
+if [[ $has_gpu ]]; then
+	run_experiment "greedy" "1" "1" "gpu"
+	run_experiment "beam" "1" "1" "gpu"
+	run_experiment "beam" "1" "100" "gpu"
+	run_experiment "beam" "2" "1" "gpu"
+	run_experiment "beam" "2" "100" "gpu"
+fi
 echo
 echo
 echo -e "$title\n$(echo -e "$result_string" | column -t -s' ')" | tee -a "${result_path}/results.log"
